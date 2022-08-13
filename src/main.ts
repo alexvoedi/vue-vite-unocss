9d94936bb313796ee3ec937db934dbc84a4bd752
Alexander Vödisch <voedisch.alexander@gmail.com>
2022-08-14 00:27:15 +0200

    chore: update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { createHead } from "@vueuse/head";
-import { router } from './router';
+import { router } from "./router";
 import { i18n } from "./i18n";
 
 import App from "./App.vue";
