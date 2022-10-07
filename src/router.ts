427ba31ef0865a8a5a06d900a7ead3d217ff11e5
Alexander Vödisch <voedisch.alexander@gmail.com>
2022-10-08 00:41:08 +0200

    fix: remove unused files

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "home",
-    component: () => import("./pages/Home.vue"),
-  }
-];
+const routes: Array<RouteRecordRaw> = [];
 
 const router = createRouter({
   history: createWebHistory(),
