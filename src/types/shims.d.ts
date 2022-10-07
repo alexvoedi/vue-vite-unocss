42c6cf691746e4a51d7056e74453b2a773342f55
Alexander Vödisch <voedisch.alexander@gmail.com>
2022-10-07 22:41:15 +0200

    fix: eslint

--- src/types/shims.d.ts
+++ src/types/shims.d.ts
@@ -2,8 +2,8 @@ declare interface Window {
   // extend the window
 }
 
-declare module '*.vue' {
-  import { type DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+declare module "*.vue" {
+  import { type DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
