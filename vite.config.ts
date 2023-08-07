74d4fa0ed9b98e8b690654c782768119f0948596
Alexander Vödisch <voedisch.alexander@gmail.com>
2023-08-07 21:50:46 +0200

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import VueI18n from '@intlify/unplugin-vue-i18n/vite'
